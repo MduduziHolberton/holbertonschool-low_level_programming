d53c65a37ae82e79dac68192d4b82e9b59f55971
SilindaM <silindanemdu@gmail.com>
2022-02-23 08:49:54 +0200

    fix 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,17 @@
 #include <main.h>
 
 void _puts_recursion(char *s){
-        printf(s,"\n");
+     if(*s=='\0'){
+       putchar('\n');
+       return;
+     }
+  putchar(*s);
+     s++;
+    _puts_recursion(s);
 }
 
 int main(void){
 _puts_recursion("mduduzi");
 }
+
 
